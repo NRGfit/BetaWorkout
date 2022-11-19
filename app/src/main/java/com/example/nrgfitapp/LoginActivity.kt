ef0a62a1dfe9e9314d9f8bc26f64896593b47a60
kavine24 <kavin.elamurugan@gmail.com>
2022-11-19 16:07:41 -0500

    Added parse configs

--- app/src/main/java/com/example/nrgfitapp/LoginActivity.kt
+++ app/src/main/java/com/example/nrgfitapp/LoginActivity.kt
@@ -1,11 +1,73 @@
 package com.example.nrgfitapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
+    val TAG = "LoginActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if(ParseUser.getCurrentUser() != null){
+            goToMainActivity()
+        }
+
+        findViewById<Button>(R.id.signInButton).setOnClickListener{
+            val username = findViewById<EditText>(R.id.login_username).text.toString()
+            val password = findViewById<EditText>(R.id.login_password).text.toString()
+
+            loginUser(username, password)
+        }
+
+        findViewById<Button>(R.id.signUpButton).setOnClickListener{
+            val username = findViewById<EditText>(R.id.login_username).text.toString()
+            val password = findViewById<EditText>(R.id.login_password).text.toString()
+
+            signUpUser(username, password)
+        }
+    }
+
+    private fun signUpUser(username: String, password: String){
+        val user = ParseUser()
+
+        user.username = username
+        user.setPassword(password)
+
+        user.signUpInBackground { e->
+            if(e == null){
+                Log.i(TAG, "Successfully signed up")
+                goToMainActivity()
+
+            }else{
+                e.printStackTrace()
+                Toast.makeText(this, "Error Signing Up", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String){
+        ParseUser.logInInBackground(username, password, ({user, e->
+            if(user != null){
+                Log.i(TAG, "Successfully logged in")
+                goToMainActivity()
+
+            }else{
+                e.printStackTrace()
+                Toast.makeText(this, "Error Logging in", Toast.LENGTH_SHORT).show()
+            }
+        }))
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
