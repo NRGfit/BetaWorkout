5aeffad89844da4b9338f7522dc9470ec64fa7c2
joeporada <poradajoe@gmail.com>
2022-11-28 13:10:00 -0500

    added basic framework for forum and profile page

---
+++ app/src/main/java/com/example/nrgfitapp/fragments/FeedFragment.kt
@@ -0,0 +1,30 @@
+package com.example.nrgfitapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nrgfitapp.R
+
+class FeedFragment : Fragment() {
+
+    lateinit var postsRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //set up views and click listeners
+
+        postsRecyclerView = view.findViewById(R.id.feedRecyclerView)
+    }
+
+}
