de4e87bf711b7c78a3900ca838be30867d84d68b
kavine24 <kavin.elamurugan@gmail.com>
2022-11-28 13:15:11 -0500

    Added Post DAO and TimeFormatter

---
+++ app/src/main/java/com/example/nrgfitapp/DAOs/Post.kt
@@ -0,0 +1,44 @@
+package com.example.nrgfitapp.DAOs
+
+import com.example.nrgfitapp.TimeFormatter
+
+
+import com.parse.ParseClassName
+import com.parse.ParseFile
+import com.parse.ParseObject
+import com.parse.ParseUser
+import java.util.*
+
+@ParseClassName("Post")
+class Post : ParseObject() {
+
+    fun getDescription(): String? {
+        return getString(KEY_DESCRIPTION)
+    }
+    fun setDescription(description: String){
+        put(KEY_DESCRIPTION, description)
+    }
+
+    fun getImage(): ParseFile? {
+        return getParseFile(KEY_IMAGE)
+    }
+    fun setImage(image: ParseFile){
+        put(KEY_IMAGE, image)
+    }
+
+    fun getUser(): ParseUser? {
+        return getParseUser(KEY_USER)
+    }
+    fun setUser(user: ParseUser){
+        put(KEY_USER, user)
+    }
+
+    fun getFormattedTimestamp(createdAt: Date): String? {
+        return TimeFormatter.getTimeDifference(createdAt.toString())
+    }
+    companion object {
+        const val KEY_DESCRIPTION = "Description"
+        const val KEY_IMAGE = "Image"
+        const val KEY_USER = "User"
+    }
+}
